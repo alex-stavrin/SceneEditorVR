3040ca00b838a5e09b7f1227653d617f482388ba
alex-stavrin <alekosstavrinoudakis@gmail.com>
2025-11-11 11:00:04 +0200

    rotation now working.

--- Assets/Scripts/Interactable/InteractableRotator.cs
+++ Assets/Scripts/Interactable/InteractableRotator.cs
@@ -10,6 +10,11 @@ public class InteractableRotator : InteractableGizmo
 
     Vector3 interactorStartingPosition;
 
+    Vector3 rotateableReferenceVector;
+
+    float startingAngle = 0;
+    Quaternion startingRotation;
+
     public void SetRotateable(Rotateable _rotateable)
     {
         rotateable = _rotateable;
@@ -21,32 +26,70 @@ public class InteractableRotator : InteractableGizmo
         {            
             Vector3 interactorCurrentPosition = interactor.gameObject.transform.position;
 
-            if(rotatingVector == new Vector3(0,1,0)) // Rotate around Y
+            Vector3 controllerDirection = (interactorCurrentPosition - interactorStartingPosition).normalized;
+            Vector3 projectedControllerDirection = Vector3.ProjectOnPlane(controllerDirection, rotatingVector);
+
+            float angle = Vector3.SignedAngle(rotateableReferenceVector, projectedControllerDirection, rotatingVector);
+
+            if (rotatingVector == new Vector3(0, 1, 0)) // y axis
             {
-                Vector3 referenceVector = rotateable.gameObject.transform.right;
+                float circleX = Mathf.Sin(angle * Mathf.Deg2Rad);
+                float circleZ = Mathf.Cos(angle * Mathf.Deg2Rad);
 
-                Vector3 controllerDirection = (interactorCurrentPosition - interactorStartingPosition).normalized;
-                Vector3 projectedControllerDirection = Vector3.ProjectOnPlane(controllerDirection, rotatingVector);
+                RotationVisualizerManager.Instance.SetTargetVisualizerLocation(interactorStartingPosition +
+                    new Vector3(0.2f * circleX, 0, 0.2f * circleZ));
 
-                float angle = Vector3.SignedAngle(projectedControllerDirection, referenceVector, rotatingVector);
-
-                float circleX = Mathf.Cos(angle * Mathf.Deg2Rad);
+                rotateable.gameObject.transform.rotation = Quaternion.AngleAxis(angle - startingAngle, new Vector3(0,1,0)) * startingRotation;
+            }
+            else if (rotatingVector == new Vector3(1, 0, 0)) // x axis
+            {
+                float circleY = Mathf.Cos(angle * Mathf.Deg2Rad);
                 float circleZ = Mathf.Sin(angle * Mathf.Deg2Rad);
 
                 RotationVisualizerManager.Instance.SetTargetVisualizerLocation(interactorStartingPosition +
-                    new Vector3(0.25f * circleX, 0, 0.25f * circleZ));
+                    new Vector3(0, 0.2f * circleY, 0.2f * circleZ));
 
-                // Vector3 rotationEuler = rotateable.gameObject.transform.rotation.eulerAngles;
-                // rotateable.gameObject.transform.rotation = Quaternion.Euler(new Vector3(rotationEuler.x, angle, rotationEuler.z));
+                rotateable.gameObject.transform.rotation = Quaternion.AngleAxis(angle - startingAngle, new Vector3(1,0,0)) * startingRotation;
+            }
+            else if (rotatingVector == new Vector3(0,0,1)) // z axis
+            {
+                float circleY = Mathf.Cos(angle * Mathf.Deg2Rad);
+                float circleX = -Mathf.Sin(angle * Mathf.Deg2Rad);
 
-                VirtualRealityConsole.PrintMessage(angle.ToString(), PrintTypeVRC.Clear);      
-            }        
+                RotationVisualizerManager.Instance.SetTargetVisualizerLocation(interactorStartingPosition +
+                    new Vector3(0.2f * circleX, circleY * 0.2f, 0));
+
+                rotateable.gameObject.transform.rotation = Quaternion.AngleAxis(angle - startingAngle, new Vector3(0,0,1)) * startingRotation;
+            }
+
+            VirtualRealityConsole.PrintMessage(angle.ToString(), PrintTypeVRC.Clear);          
         }
     }
 
     public override void OnInteractStart()
     {
         base.OnInteractStart();
+
+        if (rotateable)
+        {
+            startingRotation = rotateable.transform.rotation;
+
+            if (rotatingVector == new Vector3(0, 1, 0)) // y axis
+            {
+                startingAngle = startingRotation.eulerAngles.y;
+                rotateableReferenceVector = new Vector3(0, 0, 1);
+            }
+            else if (rotatingVector == new Vector3(1, 0, 0)) // x axis
+            {
+                startingAngle = startingRotation.eulerAngles.x;
+                rotateableReferenceVector = new Vector3(0, 1, 0);  
+            }
+            else if (rotatingVector == new Vector3(0,0,1)) // z axis
+            {
+                startingAngle = startingRotation.eulerAngles.z;
+                rotateableReferenceVector = new Vector3(0, 1, 0);  
+            }
+        }
 
         interactorStartingPosition = interactor.gameObject.transform.position;
 
--- Assets/Scripts/Managers/InspectorManager.cs
+++ Assets/Scripts/Managers/InspectorManager.cs
@@ -5,8 +5,44 @@ public class InspectorManager : MonoBehaviour
 {
     public static InspectorManager Instance { get; private set; }
 
+    [Header("General")]
+
     [SerializeField]
     TextMeshProUGUI inspectedNameText;
+
+    [Header("Location")]
+
+    [SerializeField]
+    TextMeshProUGUI labelLocationX;
+
+    [SerializeField]
+    TextMeshProUGUI labelLocationY;
+
+    [SerializeField]
+    TextMeshProUGUI labelLocationZ;
+
+    [Header("Rotation")]
+
+    [SerializeField]
+    TextMeshProUGUI labelRotationX;
+
+    [SerializeField]
+    TextMeshProUGUI labelRotationY;
+
+    [SerializeField]
+    TextMeshProUGUI labelRotationZ;
+
+    [Header("Scale")]
+    [SerializeField]
+    TextMeshProUGUI labelScaleX;
+
+    [SerializeField]
+    TextMeshProUGUI labelScaleY;
+
+    [SerializeField]
+    TextMeshProUGUI labelScaleZ;
+    
+
 
     GameObject currentInspected;
 
@@ -19,6 +55,27 @@ public class InspectorManager : MonoBehaviour
         }
 
         Instance = this;
+    }
+
+    void Update()
+    {
+        if(currentInspected)
+        {
+            Vector3 inspectedPosition = currentInspected.transform.position;
+            labelLocationX.text = inspectedPosition.x.ToString();
+            labelLocationY.text = inspectedPosition.y.ToString();
+            labelLocationZ.text = inspectedPosition.z.ToString();
+
+            Vector3 inspectedRotation = currentInspected.transform.rotation.eulerAngles;
+            labelRotationX.text = inspectedRotation.x.ToString();
+            labelRotationY.text = inspectedRotation.y.ToString();
+            labelRotationZ.text = inspectedRotation.z.ToString();
+
+            Vector3 inspectedScale = currentInspected.transform.localScale;
+            labelScaleX.text = inspectedScale.x.ToString();
+            labelScaleY.text = inspectedScale.y.ToString();
+            labelScaleZ.text = inspectedScale.z.ToString();
+        }
     }
 
     public void SetInspected(GameObject inspected)
